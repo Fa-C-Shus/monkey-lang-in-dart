63aa71211a29df69f230dcf43abf4835fddd3abd
Fa-C-Shus <facshus@gmail.com>
2023-05-26 09:54:04 -0500

    Empty starter project

---
+++ lib/monkeydart.dart
@@ -0,0 +1,10 @@
+/// monkeydart, Thorsten Ball monkey interpreter written in Dart
+///
+/// ```sh
+/// # activate monkeydart
+/// dart pub global activate monkeydart
+///
+/// # see usage
+/// mkay --help
+/// ```
+library monkeydart;
